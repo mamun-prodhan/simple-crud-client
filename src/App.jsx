6fe5c0c3f12d61c984c4d96e5d5dff21bd223b44
mamun-prodhan <mamunpr9@gmail.com>
2023-10-15 17:26:47 +0600

    form added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import "./App.css";
 
 function App() {
+  const handleAddUser = (e) => {
+    e.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = { name, email };
+    console.log(user);
+  };
   return (
     <>
       <h1>Simple Crud Client</h1>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" id="" />
+        <br />
+        <input type="email" name="email" id="" />
+        <br />
+        <input type="submit" value="Add User" />
+      </form>
     </>
   );
 }
